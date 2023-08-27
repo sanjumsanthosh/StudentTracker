22e1eacd0e53087fac59550080dc8885147a7a82
sanjay <44315700+sanjumsanthosh@users.noreply.github.com>
2023-08-27 14:24:43 +0530

    create env script

---
+++ create_env.sh
@@ -0,0 +1,20 @@
+# i need to create a .env file with config
+# DATABASE_URL="mysql://root:root@localhost:3306/school_db"
+
+# collect data from user
+# user name
+# password
+# database name
+
+# create .env file
+
+echo "Enter your database username: "
+read username
+
+echo "Enter your database password: "
+read password
+
+echo "Enter your database name: "
+read database
+
+echo "DATABASE_URL=mysql://$username:$password@localhost:3306/$database" > .env
